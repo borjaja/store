3153e37a0aa09a91b956b067565c1afa82d28b3d
Borja Alvarez <balvarez@orienteed.com>
2021-01-13 15:52:49 +0100

    error

--- pages/pokemon/[pokemon].jsx
+++ pages/pokemon/[pokemon].jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import styles from "./Pokemon.module.css";
+import styles from "./pokemon.module.css";
 
 const pokemon = ({pokemon}) => (
     <>
